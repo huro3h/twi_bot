f541b42476b7c6f7c5c74456b1fa10d5b8fb14de
huro3h <huro3h@gmail.com>
2018-02-14 08:42:15 +0900

    Create twitter_client concern for models common processing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,9 @@
 require 'twitter'
 
 class Post < ApplicationRecord
+  include TwitterClient
+
   def delete_post(delete_id)
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['CONSUMER_KEY']
-      config.consumer_secret     = ENV['CONSUMER_SECRET']
-      config.access_token        = ENV['ACCESS_TOKEN']
-      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-    end
-    @client.destroy_status(delete_id)
+    client.destroy_status(delete_id)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,12 +1,9 @@
 require 'twitter'
 
 class Tweet < ApplicationRecord
-  def initialize
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['CONSUMER_KEY']
-      config.consumer_secret     = ENV['CONSUMER_SECRET']
-      config.access_token        = ENV['ACCESS_TOKEN']
-      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-    end
+  include TwitterClient
+
+  def new
+    client
   end
 end
