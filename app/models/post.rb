de3bb387562f53381240306159d0a695e1a87049
huro3h <huro3h@gmail.com>
2018-02-15 09:01:47 +0900

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,26 @@ require 'twitter'
 class Post < ApplicationRecord
   include TwitterClient
 
-  def delete_post(delete_id)
-    client.destroy_status(delete_id)
+  def delete_oldest_post
+    oldest_post = Post.where(is_deleted: false).last
+
+    # FIX: 削除対象のツイートが既に無い場合、errorがgemから返る
+    # Twitter::Error::NotFound: No status found with that ID.
+    exec_delete(oldest_post.publish_id)
+
+    if self.was_deleted?(oldest_post.publish_id)
+      oldest_post.update(is_deleted: true, deleted_at: Time.zone.now)
+      p "#{oldest_post.id} - tweet_id: #{oldest_post.publish_id}は正常に削除されました。"
+    else
+      p "エラーが発生しました"
+    end
+  end
+
+  def exec_delete(tweet_id)
+    client.destroy_status(tweet_id)
+  end
+
+  def was_deleted?(tweet_id)
+    client.status(tweet_id).nil?
   end
 end
