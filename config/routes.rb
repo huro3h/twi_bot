3628859ccf1ea4a6ba2707b6708d7eb41e1a477d
huro3h <huro3h@gmail.com>
2018-02-20 08:32:37 +0900

    Add posts controller, actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'posts#index'
+  get 'posts/new'
+  get 'posts/show'
+  post 'posts/edit'
+  post 'posts/create'
+  post 'posts/destroy'
 end
