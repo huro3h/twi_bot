984a068cbf2276bba1156f5b59b45fac6cef4735
huro3h <huro3h@gmail.com>
2018-02-27 08:50:37 +0900

    Update rake task, cron schedule

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,10 @@ class Post < ApplicationRecord
     oldest_post_tweet_id = Post.where(is_deleted: false).last.publish_id
 
     if self.was_deleted?(oldest_post_tweet_id)
-      p "対象のツイートは既に削除されています → #{oldest_post_tweet_id}"
+      # p "対象のツイートは既に削除されています → #{oldest_post_tweet_id}"
     else
       exec_delete(oldest_post_tweet_id)
-      p "[delete_log] [#{Time.zone.now}] 削除TweetID:[#{oldest_post_tweet_id}]"
+      # p "[delete_log] [#{Time.zone.now}] 削除TweetID:[#{oldest_post_tweet_id}]"
     end
     oldest_post_tweet = Post.find_by(publish_id: oldest_post_tweet_id)
     oldest_post_tweet.update(is_deleted: true, deleted_at: Time.zone.now)
@@ -20,10 +20,10 @@ class Post < ApplicationRecord
     latest_post_tweet_id = Post.find_by(is_deleted: false).publish_id
 
     if self.was_deleted?(latest_post_tweet_id)
-      p "対象のツイートは既に削除されています → #{latest_post_tweet_id}"
+      # p "対象のツイートは既に削除されています → #{latest_post_tweet_id}"
     else
       exec_delete(latest_post_tweet_id)
-      p "[delete_log] [#{Time.zone.now}] 削除TweetID:[#{latest_post_tweet_id}]"
+      # p "[delete_log] [#{Time.zone.now}] 削除TweetID:[#{latest_post_tweet_id}]"
     end
     latest_post_tweet = Post.find_by(publish_id: latest_post_tweet_id)
     latest_post_tweet.update(is_deleted: true, deleted_at: Time.zone.now)
