6ee1a96f0e32659ccb8ea8636cbdc1c5f20a7578
huro3h <huro3h@gmail.com>
2018-03-04 20:29:31 +0900

    Fix Post#delete_latest_post

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,10 +12,10 @@ class Post < ApplicationRecord
     latest_post_tweet_id = Post.find_by(is_deleted: false).publish_id
 
     if self.was_deleted?(latest_post_tweet_id)
-      p "対象のツイートは既に削除されています → #{latest_post_tweet_id}"
+      puts "対象のツイートは既に削除されています → #{latest_post_tweet_id}"
     else
       exec_delete(latest_post_tweet_id)
-      p "[delete_log] [#{Time.zone.now}] 削除TweetID:[#{latest_post_tweet_id}]"
+      puts "[delete_log] [#{Time.zone.now}] 削除TweetID:[#{latest_post_tweet_id}]"
     end
     latest_post_tweet = Post.find_by(publish_id: latest_post_tweet_id)
     latest_post_tweet.update(is_deleted: true, deleted_at: Time.zone.now)
