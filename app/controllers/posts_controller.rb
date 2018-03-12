f8853efed275f61ec01e2a611a51733480ee3545
huro3h <huro3h@gmail.com>
2018-03-13 08:18:04 +0900

    Remove unused actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,5 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.where(is_deleted: false).order(id: :desc).limit(25)
-    # @posts = Post.page(params[:page]).per(50)
-  end
-
-  def new
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def create
-  end
-
-  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  get 'posts/new'
-  get 'posts/show'
-  post 'posts/edit'
-  post 'posts/create'
-  post 'posts/destroy'
 
   get 'tweets/index'
   get 'tweets/new'
