0357f0c19bcc0bfd4910a1ffe9e022a5cfbc976d
huro3h <huro3h@gmail.com>
2018-03-13 08:35:54 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  get 'tweets/index'
-  get 'tweets/new'
-  get 'tweets/show'
-  post 'tweets/edit'
-  post 'tweets/destroy'
+  resources :tweets, only: %i[index new show create edit destroy]
 end
