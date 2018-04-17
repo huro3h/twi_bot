6fee85fd8ffa10eec3c20d216b4ab1493207cd04
huro3h <huro3h@gmail.com>
2018-04-18 08:38:57 +0900

    To verify the validity of tweet model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe Post do
+  describe 'content' do
+  end
+end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -8,6 +8,8 @@ describe Tweet do
 
     let(:dup_tweet) { Tweet.new(content: '投稿テスト') }
     let(:tweet) { Tweet.new(content: '重複していない投稿テスト') }
+    let(:tweet_over140) { Tweet.new(content: 'a' * 141) }
+    let(:tweet_just140) { Tweet.new(content: 'a' * 140) }
 
     it '重複したcontentは無効であること' do
       expect(dup_tweet.save).to eq(false)
@@ -15,6 +17,14 @@ describe Tweet do
 
     it '重複していないcontentは有効であること' do
       expect(tweet.save).to eq(true)
+    end
+
+    it '141文字以上は入力できないこと' do
+      expect(tweet_over140.save).to eq(false)
+    end
+
+    it '140文字までは入力可能なこと' do
+      expect(tweet_just140.save).to eq(true)
     end
   end
 end
