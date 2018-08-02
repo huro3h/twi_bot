20cfca5460fb848bcffd81dd6b7ac26556ef41ef
huro3h <huro3h@gmail.com>
2018-08-02 12:40:06 +0900

    Execute rails g administrate:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :posts
+    resources :tweets
+
+    root to: 'posts#index'
+  end
+
   root to: 'posts#index'
   resources :tweets
 end
