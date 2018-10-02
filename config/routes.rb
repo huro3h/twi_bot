f1ceee5e9898cc778cad2b7eb6bfb0e06394e62b
huro3h <huro3h@gmail.com>
2018-10-02 18:29:05 +0900

    Uninstall gem administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :posts
-    resources :tweets
-
-    root to: 'posts#index'
-  end
-
   root to: 'posts#index'
   resources :tweets
 end
