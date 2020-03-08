0a6facf717f5eb1268b57372e55caec9459f9afa
huro3h <huro3h@gmail.com>
2020-03-08 15:15:59 +0900

    :+1: コールバックを定義した

--- app/models/music.rb
+++ app/models/music.rb
@@ -6,4 +6,35 @@ class Music
   ATTRIBUTES = %i[title created_at listened_at]
 
   attr_accessor *ATTRIBUTES
+
+  # コールバックを定義
+  define_model_callbacks :create, :play
+
+  before_play :display_title
+  after_create ->(music) { music.created_at = Time.current }
+
+  def self.create(title: nil)
+    music = new
+    music.title = title
+    music.create
+  end
+
+  def create
+    run_callbacks :create do
+      puts 'つくりました'
+      self
+    end
+  end
+
+  def play
+    run_callbacks :play do
+      puts '今から曲流しますよー'
+    end
+  end
+
+  private
+
+  def display_title
+    puts title
+  end
 end
