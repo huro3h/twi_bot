3bab90362be43ea063830e2244910aa20e36202c
huro3h <huro3h@gmail.com>
2020-03-14 13:16:12 +0900

    :+1: Add description

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,6 +1,7 @@
 class Computer
   include ActiveModel::Dirty
 
+  # 変更を検知したい属性名を指定
   define_attribute_methods :name
 
   # getter
@@ -10,12 +11,16 @@ class Computer
 
   # setter
   def name=(value)
+    # xxx_will_change! 特定のカラムの変更状態を追跡
     name_will_change! unless value == @name
     @name = value
   end
 
   def save
+    # 前回の変更箇所と値を @previously_changed に保持
     @previously_changed = changes
+
+    # 状態のクリア
     @changed_attributes.clear
   end
 end
