8c2f9fd60d9c4c740ff1004ede83e4121d49cbaf
huro3h <huro3h@gmail.com>
2020-03-14 12:58:08 +0900

    :+1: Create Computer class with ActiveModel::Dirty

---
+++ app/models/computer.rb
@@ -0,0 +1,21 @@
+class Computer
+  include ActiveModel::Dirty
+
+  difine_attribute_methods :name
+
+  # getter
+  def name
+    @name
+  end
+
+  # setter
+  def name=(value)
+    name_will_change! unless value == @name
+    @name = val
+  end
+
+  def save
+    @previously_changed = changes
+    @changed_attributes.clear
+  end
+end
